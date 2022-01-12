28d48fc5e7f44c4731e653bcd3105e6ba7098132
AswinBot <97603775+AswinBot@users.noreply.github.com>
2022-01-12 20:50:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -43,7 +43,7 @@ Developer is Super Noob. Just Learning from Official Docs. Please Donate the dev
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](r.aswin2020@oksbi) (Gpay)
+[Donate Now](r.aswin2020@oksbi) (Gpay r.aswin2020@oksbi)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
